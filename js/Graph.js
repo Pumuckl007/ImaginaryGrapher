8bb7e874cfdb9a563cdf9475f1a9a3aa4139a454
Pumuckl007 <example@example.com>
2016-03-12 18:05:02 -0800

    commit befor starting work today

--- js/Graph.js
+++ js/Graph.js
@@ -1,9 +1,20 @@
 "use strict";
 class Graph{
   constructor(domElement){
-    this.canvas = domElement;
+    this.dom = domElement;
+    console.log(this);
+    this.canvas = domElement.firstElementChild;
     this.ctx = this.canvas.getContext('2d');
     this.points = [];
+    var self = this;
+    this.canvas.width = this.dom.offsetWidth;
+    this.canvas.height = this.dom.offsetHeight;
+    this.canvas.addEventListener('click', function() {
+      self.points.push({x:Math.random()*100, y:Math.random()*100});
+      self.points.push({x:Math.random()*100, y:Math.random()*100});
+      self.points.push({x:Math.random()*100, y:Math.random()*100});
+      self.render();
+     }, false);
   }
 
   fit(){
@@ -16,20 +27,23 @@ class Graph{
   }
 
   render(){
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     let zAndC = this.getZoomAndCenter();
     let zoom = zAndC.zoom;
     let center = zAndC.center;
     this.ctx.save();
+    this.ctx.translate(Math.random(), 0);
     this.ctx.scale(zoom, zoom);
+    console.log(center.x, center.y);
+    this.ctx.fillStyle = "orange";
     this.ctx.beginPath();
     if(this.points[0]){
       this.ctx.moveTo(this.points[0].x, this.points[0].y);
-      for(let point in this.points){
+      for(let point of this.points){
         this.ctx.lineTo(point.x, point.y);
       }
       this.ctx.lineTo(this.points[0].x, this.points[0].y);
     }
-    this.ctx.closePath();
     this.ctx.fill();
     this.ctx.restore();
   }
@@ -41,8 +55,8 @@ class Graph{
   getZoomAndCenter(){
     let max = this.getMax();
     let aspect = this.getAspect();
-    let width = max[2]-max[0];
-    let height = max[3]-max[1];
+    let width = max[1]-max[0];
+    let height = max[3]-max[2];
     let center = {x:width/2, y:height/2};
     let scaleMax = Math.max(width, height*aspect);
     let pixleWidth = this.canvas.offsetWidth;
@@ -61,3 +75,4 @@ class Graph{
     return max;
   }
 }
+var graph = new Graph(document.getElementById("canvasWrap"));
